1db4521603233d8b5ce283dd69857db02da738fb
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-05-09 14:18:17 +0300

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    else:
+        return number / 2
+
+    
+print(greet_programmer())
+print(greet("Naureen"))
+print(greet_with_default())
+print(greet_with_default("Sunny")) 
+print(add(45,55))  
+print(halve(4))
+print(halve("two"))
